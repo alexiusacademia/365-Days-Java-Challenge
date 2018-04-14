94ad6716e4ad9a9263d5da657409881d13a00562
syncster31 <alexius.academia@gmail.com>
2018-04-14 13:14:46 +0800

    Inputs from the user

---
+++ Day2/inputs/Inputs2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Inputs2 {
+	public static void main(String[] args) throws IOException {
+		// The object for reading
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(System.in)
+			);
+
+		System.out.print("Enter more than one integer: ");
+		// Takes a string and splits with whitespace
+		String[] inputs = reader.readLine().split(" ");
+		int num1 = Integer.parseInt(inputs[0]);  // Convert the string to integer
+		int num2 = Integer.parseInt(inputs[1]);  //    -do-
+
+		System.out.println(num1 + ", " + num2);
+	}
+}
