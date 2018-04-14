e16ed450314e1db5583176e0ad8ef5758d58d3d8
syncster31 <alexius.academia@gmail.com>
2018-04-14 10:44:26 +0800

    Inline comment

---
+++ Day2/Comments/InlineComments.java
@@ -0,0 +1,10 @@
+public class InlineComments
+{
+
+	public static void main(String[] args)
+	{
+		// This is a comment
+		System.out.println("If line above is commented, this will compile.");
+	}
+
+}
