4d33ebdea2a5dc23bb4e7177ef18bc07adaaa294
syncster31 <alexius.academia@gmail.com>
2018-04-14 12:21:56 +0800

    runtime error and commandline arguments

---
+++ Day2/commandline-arguments/CLA1.java
@@ -0,0 +1,8 @@
+// Command line arguments
+
+public class CLA1{
+	public static void main(String[] args){
+		// Prints the number of commandline arguments
+		System.out.println("Number of arguments = " + args.length);
+	}
+}
---
+++ Day2/runtime-error/Task.java
@@ -0,0 +1,10 @@
+// This will compile but will throw error at runtime
+
+public class Task {
+  public static void main(String args) throws Exception {
+    // put your code here
+  }
+  public void main(String[] args) throws Exception {
+    System.out.println("Test");
+  }
+}
