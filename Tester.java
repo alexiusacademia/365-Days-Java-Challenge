9ad103b8a7fe8b8034e7f83de0463fafeacff57b
syncster31 <alexius.academia@gmail.com>
2018-04-22 10:47:32 +0800

    Added a tester file.

---
+++ Tester.java
@@ -0,0 +1,8 @@
+import testpackage.*;
+
+public class Tester {
+	public static void main(String[] args) {
+		Test t = new Test(1, "Alex");
+		System.out.println(t.getName());
+	}
+}
---
+++ testpackage/Test.java
@@ -0,0 +1,19 @@
+package testpackage;
+
+public class Test {
+	private int id;
+	private String name;
+
+	public Test(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
