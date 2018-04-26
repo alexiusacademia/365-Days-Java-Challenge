bc129d68a46e3aeda81eaee67b05147af0636d85
syncster31 <alexius.academia@gmail.com>
2018-04-27 07:12:27 +0800

    Day15 - Throw Exception

---
+++ Day15/ThrowException.java
@@ -0,0 +1,13 @@
+public class ThrowException {
+  public static void main(String[] args) {
+    validateAge(16);
+  }
+
+  private static void validateAge(int age) {
+    if (age < 18) {
+      throw new ArithmeticException("Can't join right now.");
+    } else {
+      System.out.println("Welcome to the gang!");
+    }
+  }
+}
