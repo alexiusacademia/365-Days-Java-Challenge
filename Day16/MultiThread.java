67d6754461ba6530fc712dde4d3395c5e80218c0
syncster31 <alexius.academia@gmail.com>
2018-04-28 18:37:30 +0800

    Starting multithreading

---
+++ Day16/MultiThread.java
@@ -0,0 +1,41 @@
+/**
+ * Demonstrating use of a thread
+ */
+public class MultiThread {
+  public static void main(String[] args) {
+    SampleThread st = new SampleThread("MyThread");
+    st.start();
+  }
+}
+// Thread class
+class SampleThread implements Runnable {
+  private Thread t;
+  private String threadName;
+
+  SampleThread(String name) {
+    this.threadName = name;
+    System.out.println("Creating " + this.threadName);
+  }
+
+  public void run() {
+    System.out.println("Running " + this.threadName);
+
+    try {
+      for (int i=10; i>0; i--) {
+        System.out.println("Thread: " + this.threadName + ", " + i);
+        Thread.sleep(50);
+      }
+    } catch (InterruptedException e) {
+      System.out.println("Thread " + this.threadName + " interrupted.");
+    }
+    System.out.println("Thread " + this.threadName + " exiting...");
+  }
+
+  public void start() {
+    System.out.println("Starting " + this.threadName);
+    if (this.t == null) {
+      this.t = new Thread(this, this.threadName);
+      this.t.start();
+    }
+  }
+}
