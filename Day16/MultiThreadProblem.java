9d61e14f5e2e240a0578d2fe5718b05fd02626e9
syncster31 <alexius.academia@gmail.com>
2018-04-28 19:45:53 +0800

    Day 16 - Shows multithreading problems

---
+++ Day16/MultiThreadProblem.java
@@ -0,0 +1,57 @@
+/**
+ * This mulithread demo will show unexpected results
+ */
+
+public class MultiThreadProblem {
+  public static void main(String[] args) {
+    MyObject obj = new MyObject();
+
+    ThreadDemo td1 = new ThreadDemo("Thread 1 ", obj);
+    ThreadDemo td2 = new ThreadDemo("Thread 2 ", obj);
+
+    td1.start();
+    td2.start();
+
+    try {
+      td1.join();
+      td2.join();
+    } catch (Exception e) {
+      System.out.println(e.toString());
+    }
+  }
+}
+
+class MyObject {
+  public void printCount() {
+    try {
+      for (int i=0; i<10; i++) {
+        System.out.println("Counter --- " + i);
+      }
+    } catch (Exception e) {
+      System.out.println(e.toString());
+    }
+  }
+}
+
+class ThreadDemo extends Thread {
+  private Thread thread;
+  private String threadName;
+  MyObject obj;
+
+  ThreadDemo(String name, MyObject o) {
+    this.threadName = name;
+    this.obj = o;
+  }
+
+  public void run() {
+    obj.printCount();
+    System.out.println("Thread " + this.threadName + " exiting...");
+  }
+
+  public void start() {
+    if (this.thread == null) {
+      this.thread = new Thread(this, this.threadName);
+      this.thread.start();
+    }
+  }
+}
